d63d2d8200317532350c109fb829dc01ca743a7f
Alex Lyakas <alex@zadarastorage.com>
2014-06-19 22:01:03 +0300

    run_vdbench: allow specifying iorate for the test

--- run_vdbench.py
+++ run_vdbench.py
@@ -15,9 +15,9 @@ def usage_and_exit(msg, parser):
 
 def gen_out_dir(opts):
     return os.path.join(opts.out_root_dir, 
-        '{0}__O{1}_r{2}_s{3}_x{4}_e{5}'.format(time.strftime("%Y-%m-%d__%H-%M-%S"),
+        '{0}__O{1}_r{2}_s{3}_x{4}_i{5}_e{6}'.format(time.strftime("%Y-%m-%d__%H-%M-%S"),
             opts.outstanding, opts.readpct, opts.seekpct, opts.xfersize,
-            opts.elapsed))
+            opts.iorate, opts.elapsed))
 
 def prepare_vdbench_input_file(fname, opts):
     with open(fname, 'w') as f:
@@ -26,17 +26,18 @@ def prepare_vdbench_input_file(fname, opts):
             f.write('sd=sd{0},lun={1},threads={2},openflags=o_direct\n'.format(sd_idx, blkdev, opts.outstanding))
             sd_idx = sd_idx + 1
         f.write('wd=wd1,sd=(sd*),rdpct={0},seekpct={1},xfersize={2}k\n'.format(opts.readpct, opts.seekpct, opts.xfersize))
-        f.write('rd=run_vdbench,wd=(wd1),iorate=max,elapsed={0},interval=1\n'.format(opts.elapsed))
+        f.write('rd=run_vdbench,wd=(wd1),iorate={0},elapsed={1},interval=1\n'.format(opts.iorate, opts.elapsed))
         f.flush()
 
 def main():
     parser = argparse.ArgumentParser(description='Run basic vdbench test on a block device')
     parser.add_argument('blkdevs', nargs='+', help='block devices to run the test on')
-    parser.add_argument('-O', '--outstanding', type=int, default=32, help='number of outstanding IOs')
-    parser.add_argument('-r', '--readpct', type=int, default=0, help='read percentage')
-    parser.add_argument('-s', '--seekpct', type=int, default=100, help='seek percentage (random vs sequential)')
-    parser.add_argument('-x', '--xfersize', type=int, default=4, help='transfer size in KB')
-    parser.add_argument('-e', '--elapsed', type=int, default=30, help='duration of the test')
+    parser.add_argument('-O', '--outstanding', type=int, default=32, help='number of outstanding IOs, default is 32')
+    parser.add_argument('-r', '--readpct', type=int, default=0, help='read percentage, default is 0')
+    parser.add_argument('-s', '--seekpct', type=int, default=100, help='seek percentage (random vs sequential), default is 100')
+    parser.add_argument('-x', '--xfersize', type=int, default=4, help='transfer size in KB, default is 4')
+    parser.add_argument('-i', '--iorate', default='max', help='IOPS for the test, can be \'max\', \'curve\' or a number, default is \'max\'')
+    parser.add_argument('-e', '--elapsed', type=int, default=30, help='duration of the test in seconds, default is 30')
     parser.add_argument('-o', '--out_root_dir', default=os.getcwd(), help='directory in which auto-generated output directories will be created')
     parser.add_argument('--exact_out_dir', help='exact directory, in which test output will be (overrides --out_root_dir)')
     parser.add_argument('-v', '--vdbench', default=os.path.join(os.getcwd(), 'vdbench'), help='path to the vdbench run-script')
@@ -53,6 +54,13 @@ def main():
         usage_and_exit('--xfersize must be positive', parser)
     if opts.elapsed <= 0:
         usage_and_exit('--elapsed must be positive', parser)
+    if opts.iorate != 'max' and opts.iorate != 'curve':
+        try:
+            val = int(opts.iorate)
+            if val <= 0:
+                raise Exception('iorate is not positive')
+        except Exception:
+            usage_and_exit('--iorate must be \'max\', \'curve\' or a positive integer', parser)
 
     # figure out the output dir for the test
     out_dir = ""
